fe2cf3d87f553eff63c1cd8a02cd48db93b7452a
Piotr <pitbroo@gmail.com>
2021-12-05 19:32:34 +0100

    change logo and image

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -6,6 +6,7 @@ import { takeUntil } from 'rxjs/operators';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/auth/account.model';
 
+
 @Component({
   selector: 'jhi-home',
   templateUrl: './home.component.html',
